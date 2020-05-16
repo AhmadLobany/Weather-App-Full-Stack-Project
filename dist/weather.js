f471cb2def3a3d6524ccc08fe3f8f457e0489ac6
AhmadLobany <ahmad.lobany@campus.technion.ac.il>
2020-05-16 12:05:47 +0300

    commit

--- dist/weather.js
+++ dist/weather.js
@@ -16,11 +16,10 @@ class WeatherApp {
 
     async getCityData(cityName) {
         //check if already exist 
-        if(this.cityData.length>0) {
-        const cityEle = this.cityData.find(u => u.name.toLowerCase() == cityName.toLowerCase())
-        } else {
-            const cityEle = undefined
-        }
+        let cityEle = undefined
+        if (this.cityData.length > 0) {
+            cityEle = this.cityData.find(u => u.name.toLowerCase() == cityName.toLowerCase())
+        } 
         if (cityEle == undefined) {
             let city = await $.get(`/city/${cityName}`)
             this.cityData.push(city)
